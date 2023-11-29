1509cee8cdde00887085c9a5fc9377fe6d31a8f8
DivineDK <divinedorvlo@yahoo.com>
2023-11-28 16:36:40 -0800

    8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
-def to_uper(character):
-    if ord(character) >= 97 and ord(character) <= 122:
-        return chr(ord(character) - 32) 
-    else:
-        return character 
-
-
 def uppercase(str):
-    new = ""
-    for character in str:
-        new += to_uper(character)
-    print("{:s}".format(new))
-
+    for i in str:
+        if ord("a") <= ord(i) <= ord("z"):
+            i = chr(ord(i) - 32)
+        print("{:s}".format(i), end="")
+    print()
